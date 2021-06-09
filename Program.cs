7e4a300ca9c5bba6ebdf894a44785b0c189b4b02
BrunoWM <bwillmoura3595@gmail.com>
2021-06-09 18:34:45 -0300

    criação metodo ObterOpcaoUsuario program.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace DIO_Bank_DotNET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Conta bruno = new Conta("Bruno W.", TipoConta.PessoaFisica, 1000, 1000);
+            //Console.WriteLine(bruno.ToString());
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            string menu = "";
+            menu += "\n\r WILL Bancos ao seu dispor!";
+            menu += "\n\r Informe a opção desejada:";
+            menu += "\n\r 1 - Listar contas.";
+            menu += "\n\r 2 - Inserir nova conta.";
+            menu += "\n\r 3 - Transferir.";
+            menu += "\n\r 4 - Sacar.";
+            menu += "\n\r 5 - Depositar.";
+            menu += "\n\r C - Limpar tela.";
+            menu += "\n\r X - Sair. \n\r";
+            Console.WriteLine(menu);
+            
+            string opcUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+
+            return opcUsuario;
         }
     }
 }
